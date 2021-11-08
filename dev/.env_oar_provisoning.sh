3749d5933589cd549ed6a15f0d58ec1dad32a7ce
Faure Adrien <adrien.faure2@gmail.com>
2021-11-08 13:44:53 +0100

    [dev] env provision example

--- dev/.env_oar_provisoning.sh
+++ dev/.env_oar_provisoning.sh
@@ -8,3 +8,10 @@ FRONTEND_OAREXEC=true
 #TARBALL="https://github.com/oar-team/oar/archive/refs/heads/master.tar.gz"
 #TARBALL="https://github.com/oar-team/oar3/archive/refs/heads/master.tar.gz"
 #TARBALL="https://github.com/oar-team/oar3/archive/3b06b64a4dbec62f1017963b62d619686c190a88.tar.gz"
+
+# Config for installing oar2 with oar3
+# Note that SRC must point to an oar2 folder
+# The running oar server uses oar2, but one can use the oar3 scheduler
+# MIXED_INSTALL=true
+# SRC_OAR3=oar3
+#TARBALL_OAR3="https://github.com/oar-team/oar3/archive/refs/heads/master.tar.gz"
